0a633ce8de48593f5d6dc48e60d860b0e74c7491
Vevaan Verma <vevaan.s.verma@gmail.com>
2024-01-25 15:18:04 -0800

    Add project

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    [Header("Role Selector")]
+    [SerializeField] private RoleButton defenderButton;
+    [SerializeField] private RoleButton hackerButton;
+
+    private void Start() {
+
+    }
+}
---
+++ Assets/Scripts/RoleButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoleButton : Button {
+
+    [Header("Role")]
+    [SerializeField] private RoleType roleType;
+
+}
